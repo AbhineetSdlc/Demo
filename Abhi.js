dc0c2a6c2e39417ff5d9498f0af1dd0d5ac8380c
mohammad-sdlc <mohammad@sdlccorp.com>
2023-06-28 15:16:38 +0530

    updated

---
+++ Abhi.js
@@ -0,0 +1,81 @@
+// const add = (a,b)=>{
+//     return a+b;
+// }
+
+// const sub = (a,b)=>{
+//     return Math.abs(a-b);
+
+// }
+// const mul = (a,b)=>{
+//     return a*b;
+// }
+
+// const div = (a,b)=>{
+//     return a/b;
+// }
+
+// const cal = (a,b,opt)=>{
+//     return opt(a,b);
+
+// }
+
+// console.log(cal(6,8,add));
+
+var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+// exports.myDateTime = function () {
+//     return Date();
+// };
+
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8085); //the server object listens on port 8080
+
+// var url = require('url');
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// var q = url.parse(adr, true);
+
+// console.log(q.host); //returns 'localhost:8080'
+// console.log(q.pathname); //returns '/default.htm'
+// console.log(q.search); //returns '?year=2017&month=february'
+
+// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log(qdata.month); //returns 'february'
+
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8088);
+
+
+var http = require('http');
+var uc = require('upper-case');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(8088);
+
